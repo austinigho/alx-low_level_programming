20e3eb93e38ce85e3837a0e71a9f72cdf054c175
austinigho <austinomd@gmail.com>
2022-12-09 04:47:36 -0800

    my hello world task 4 commit

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 /**
- * main - printing
- *
- * Return: Always 0 (Successful)
+ * main - Entry point
+ * Description: prints "Programming is like building a multilingual puzzle"
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
-	puts("/"Programming is like building a multilingual puzzle");
+	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
-}	
+}
